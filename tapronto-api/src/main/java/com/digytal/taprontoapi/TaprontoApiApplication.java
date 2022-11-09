4d3308c143fd6b32aeda2a136352aed5a6e85286
Gleyson Sampaio <glysns@hotmail.com>
2022-11-09 16:34:18 -0300

    projeto spring boot

---
+++ tapronto-api/src/main/java/com/digytal/taprontoapi/TaprontoApiApplication.java
@@ -0,0 +1,13 @@
+package com.digytal.taprontoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaprontoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaprontoApiApplication.class, args);
+	}
+
+}
