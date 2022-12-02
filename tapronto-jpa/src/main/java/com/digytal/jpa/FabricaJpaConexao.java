d9b9f4c1a31c476f406e329e6bf95bc274290812
Gleyson Sampaio <glysns@hotmail.com>
2022-12-02 11:03:05 -0300

    CRUD com JPA

---
+++ tapronto-jpa/src/main/java/com/digytal/jpa/FabricaJpaConexao.java
@@ -0,0 +1,21 @@
+package com.digytal.jpa;
+
+import javax.persistence.*;
+
+public class FabricaJpaConexao {
+    //conexao
+    private static EntityManager entityManager;
+    public static void conectar() {
+        try {
+            EntityManagerFactory factory = Persistence.createEntityManagerFactory("MY_PU");
+            entityManager = factory.createEntityManager();
+            System.out.println("conexao realizada com sucesso");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        FabricaJpaConexao.conectar();
+    }
+}
